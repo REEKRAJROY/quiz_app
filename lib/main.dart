57202d6472db8c45fbe7af90d01c88e5851e7552
Reekraj Roy <55712612+REEKRAJROY@users.noreply.github.com>
2021-07-11 21:25:34 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer 1 chosen!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = ['What is favourite color?', 'What is favourite animal?'];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Welcome to quiz app!')),
+        body: Column(children: [
+          Text(questions[0]),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: () => print('Answer 2 chosen!'),
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: () {
+              //...
+              print('answer 3 chosen!');
+            },
+          ),
+        ]),
+      ),
+    );
+  }
+}
